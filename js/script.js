8c5b91db6a2eb9ebe0e234f6a92f3961de2a84b8
y2onsoo <abbcaog000@naver.com>
2023-05-15 10:41:45 +0900

    mbti, 스킬영역 수정

--- js/script.js
+++ js/script.js
@@ -134,4 +134,17 @@ function btn() {
 
 
 
-////////
+////////////타이핑효과
+const content = "Typing Effect"
+const text = document.querySelector(".text")
+let index = 0;
+
+function typing(){
+  text.textContent += content[index++];
+  if(index > content.length){
+    text.textContent = ""
+    index = 0;
+  }
+}
+
+setInterval(typing, 500)
